7ffae0f750cd2424f2b6c4d18f2fdeb8a0b6a36b
Precious Airh <68897397+airhunmwup@users.noreply.github.com>
2021-12-01 18:03:26 +0000

    Update events.vue

--- src/views/events.vue
+++ src/views/events.vue
@@ -1,42 +1,21 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container">
+    <div class="row g-2 justify-content-center rounded">
+      <div class="col-12 col-md-6 pt-4">
+        <div class="container">
+          <div class="justify-content-center p-4">
+            <figure>
+              <blockquote class="blockquote">
+                <p>More Features Coming Soon!</p>
+              </blockquote>
+              <figcaption class="blockquote-footer text-info">
+                would be restricted to
+                <cite title="Source Title font-weight-bold">Membership.</cite>
+              </figcaption>
+            </figure>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
-
-<script>   
-var axios = require("axios").default;
-export default {
-  props: {
-    
-  },
-  data() {
-    return {
-      events: '',
-    };
-  },
-methods: {  
-getEvents(){    
-var options = {
-  method: 'GET',
-  url: 'https://api-football-v1.p.rapidapi.com/v3/fixtures/events',
-  params: {fixture: '215662'},
-  headers: {
-    'x-rapidapi-host': 'api-football-v1.p.rapidapi.com',
-    'x-rapidapi-key': 'cfe1fd719emshc0e80154b5d46f5p143b83jsnb450ad925850'
-  }
-};
-
-axios.request(options).then((response) => {
-    this.events = response.data.response;
-	console.log(this.events);
-}).catch(function (error) {
-	console.log(error);
-});
-},
-},
-beforeMount() {
-     this.getEvents();
-  },
-}
-</script>
