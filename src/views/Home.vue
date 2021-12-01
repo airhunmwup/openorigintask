884bde6c016e7429b0957d5024cc9e5b1fee5f4d
Precious Airh <68897397+airhunmwup@users.noreply.github.com>
2021-12-01 17:59:52 +0000

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,42 @@
 <template>
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="container overflow-hidden">
+      <div class="">
+        <div class="row justify-content-center p-2">
+          <div class="col-sm-6 col-md-4">
+            <router-link to="/login" class="text-dark">
+              <div
+                class="
+                  p-2
+                  bg-gradient
+                  text-center
+                  rounded
+                  shadow
+                  bg-warning
+                  font-weight-bold
+                  border
+                "
+              >
+                <span class="h3">Membership</span
+                ><span class="fa fa-lock m-1"></span></div
+            ></router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+<Live/>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Live from "@/views/live.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    Live,
+  },
+};
 </script>
