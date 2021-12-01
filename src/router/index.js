6235430fa44cde537d199e1de049b0f2b7faa08e
Precious Airh <68897397+airhunmwup@users.noreply.github.com>
2021-12-01 18:01:43 +0000

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
-import Events from '../views/events.vue'
-import Live from '../views/live.vue'
-import Teams from '../views/teams.vue'
-import Leagues from '../views/leagues.vue'
+import Login from '../views/login.vue'
 
 const routes = [
   {
@@ -12,37 +9,17 @@ const routes = [
     component: Home
   },
   {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/events',
-    name: 'events',
-    component: Events
+    path: '/login',
+    name: 'login',
+    component: Login
   },
   {
     path: '/live',
     name: 'live',
-    component: Live
-  },
-  {
-    path: '/teams',
-    name: 'teams',
-    component: Teams
-  },
-  {
-    path: '/leagues',
-    name: 'leagues',
-    component: Leagues
-  },
-  {
-    path: '/events',
-    name: 'Events',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/events.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/live.vue')
   }
 ]
 
